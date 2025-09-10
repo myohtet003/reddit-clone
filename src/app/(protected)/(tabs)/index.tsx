ebac6c6b86f3422d99bcd46456d65e1f2b50a10f
myohtet003 <myohtetkyaw2003@gmail.com>
2025-09-10 23:35:20 +0630

    fetch data and supabase types

--- src/app/(protected)/(tabs)/index.tsx
+++ src/app/(protected)/(tabs)/index.tsx
@@ -1,11 +1,39 @@
 import { View, Text, Image, StyleSheet, FlatList } from "react-native";
-import React from "react"; 
-import PostListItem from "../../../components/PostListItem";
+import React, {useState, useEffect} from "react"; 
 
-import posts from "../../../../assets/data/posts.json";
+import PostListItem from "../../../components/PostListItem";
+import { supabase } from "../../../lib/superbase";
+import { Tables } from "../../../types/database.types";
+
+type Post = Tables<"posts"> & {
+	user: Tables<"users">;
+	group: Tables<"groups">;
+};
+
+// import posts from "../../../../assets/data/posts.json";
 
 
 export default function HomeScreen() {
+
+	  const [posts, setPosts] = useState<Post[]>([]);
+
+	  useEffect(() => {
+	    fetchPosts();
+	  }, []);
+
+	  const fetchPosts = async () => {
+	    const { data, error } = await supabase
+	      .from('posts')
+	      .select('*, group:groups(*), user:users!posts_user_id_fkey(*)');
+		//   console.log("error", error);
+		//   console.log("data", JSON.stringify(data,null, 2));
+		if(error) {
+			console.log("Error fetching posts:", error.message);
+			return;
+		} else {
+			setPosts(data);
+		}
+	  }
   
   return (
     <View>
